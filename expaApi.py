67c06eece3b4726650e8dbc15740beb8850b8179
Camilo Forero <ce.forero2551@uniandes.edu.co>
2016-01-24 16:55:39 -0500

    Added some more analytics functionality to get all current year's total exchanges for all entities in the world

--- expaApi.py
+++ expaApi.py
@@ -146,7 +146,10 @@ class ExpaApi:
         """
             Extrae el ip/ma/re de un mes específico, en un año específico, para un comité y uno de los 4 programas
         """
-        weekStart = datetime.strptime('%d %d 1' % (year, week), '%Y %W %w').strftime('%Y-%m-%d')
+        if week == 0:
+            weekStart = "%d-01-01" % year
+        else:
+            weekStart = datetime.strptime('%d %d 1' % (year, week), '%Y %W %w').strftime('%Y-%m-%d')
 
         weekEnd = datetime.strptime('%d %d 0' % (year, week), '%Y %W %w').strftime('%Y-%m-%d')
 
@@ -164,7 +167,32 @@ class ExpaApi:
             print json.loads(requests.get(query).text)
         return {'MA': response['total_approvals']['doc_count'], 'RE': response['total_realized']['doc_count']}
 
+    def getLCWeeklyPerformance(self, lc=1395):
+        now = datetime.now()
+        currentWeek = int(now.strftime('%W'))
+        currentYear = int(now.strftime('%Y'))
+        answer = {}
+        for io in ['i', 'o']:
+            for program in ['gcdp', 'gip']:
+                ma = []
+                re = []
+                maTotal = 0
+                reTotal = 0
+                for i in range (currentWeek+1):
+                    weekData = self.getWeekStats(i, currentYear, io+program, lc)
+                    ma.append(weekData['MA'])
+                    maTotal += weekData['MA']
+                    re.append(weekData['RE'])
+                    reTotal += weekData['RE']
+                totals = {'MATOTAL':maTotal, 'RETOTAL':reTotal}
+                weekly = {'MA': ma, 'RE': re}
+                answer[io+program] = {'totals': totals, 'weekly': weekly}
+        return answer
+
     def getLCYearlyPerformance(self, year, lc=1395):
+        """
+            Returna el desempeño en matches y realizaciones de un LC en un año dado, separado por mes, para los cuatro programas
+        """
         answer = {}
         for io in ['i', 'o']:
             for program in ['gcdp', 'gip']:
@@ -179,3 +207,81 @@ class ExpaApi:
                 answer[io+program] = {'MA': ma, 'RE': re}
         return answer
         
+#Métodos relacionados con el año actual
+
+    def getCurrentYearStats(self, program, lc = 1395):
+        """
+            Extrae el ma/re de el año actual, para un comité y uno de los 4 programas
+        """
+        now = datetime.now()
+        currentYear = int(now.strftime('%Y'))
+        startDate = "%d-01-01" % currentYear 
+
+        endDate = now.strftime('%Y-%m-%d')
+
+        queryArgs = {
+            'basic[home_office_id]':lc, 
+            'basic[type]':self.ioDict[program[0]], 
+            'end_date':endDate, 
+            'programmes[]':self.programDict[program[1:]], 
+            'start_date':startDate
+        }
+        query = self._buildQuery(['applications', 'analyze.json'], queryArgs)
+        try:
+            response = json.loads(requests.get(query).text)['analytics']
+        except:
+            print json.loads(requests.get(query).text)
+        return {'MA': response['total_approvals']['doc_count'], 'RE': response['total_realized']['doc_count']}
+
+    def getCountryCurrentYearStats(self, program, lc = 1551):
+        """
+            Extrae el ma/re de el año actual, para un comité y uno de los 4 programas
+        """
+        now = datetime.now()
+        currentYear = int(now.strftime('%Y'))
+        startDate = "%d-01-01" % currentYear 
+
+        endDate = now.strftime('%Y-%m-%d')
+
+        queryArgs = {
+            'basic[home_office_id]':lc, 
+            'basic[type]':self.ioDict[program[0]], 
+            'end_date':endDate, 
+            'programmes[]':self.programDict[program[1:]], 
+            'start_date':startDate
+        }
+        query = self._buildQuery(['applications', 'analyze.json'], queryArgs)
+        try:
+            lcData = json.loads(requests.get(query).text)['analytics']['children']['buckets']
+            response = {}
+            for lc in lcData:
+                response[lc['key']] = {'MA': lc['total_approvals']['doc_count'], 'RE': lc['total_realized']['doc_count']}
+
+        except Exception as e:
+            print e
+            print json.loads(requests.get(query).text)
+        return response
+
+#Listas de MCs, LCs, regiones y similares
+
+    def getRegions(self):
+        """
+            Gets the information of all AIESEC regions. 1626 is the EXPA id of AIESEC INTERNATIONAL; all regions appear as suboffices
+        """
+        query = self._buildQuery(['committees', '1626.json'])
+        return json.loads(requests.get(query).text)['suboffices']
+
+    def getMCs(self, region):
+        """
+        Gets the information of all countries inside a given AIESEC region, whose ID enters as a parameter
+        """
+        query = self._buildQuery(['committees', '%d.json' % region])
+        return json.loads(requests.get(query).text)['suboffices']
+
+    def getSuboffices(self, subofficeID):
+        """
+        Gets the information of all countries inside a given AIESEC region, whose ID enters as a parameter
+        """
+        query = self._buildQuery(['committees', '%d.json' % subofficeID])
+        return json.loads(requests.get(query).text)['suboffices']
+
