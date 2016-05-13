094069807b53c81688dd213480007da1bf21ee04
Ubuntu <ubuntu@ip-172-31-30-126.us-west-2.compute.internal>
2016-05-13 05:37:13 +0000

    Added unicode compatibility to the settings file

--- example_settings.py
+++ example_settings.py
@@ -1,1 +1,4 @@
+#coding: utf-8
+from __future__ import unicode_literals
+
 DEFAULT_ACCOUNT = 'sebastian.ramirezc@aiesec.net' #The account that will be used, by default, to use the API. Its password should be saved in the database, using the admin interface
