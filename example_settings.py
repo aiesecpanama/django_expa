19b23ac5d9c639a60538f7027d2c3e54354178c7
AIESEC en Panamá <panama@aiesec.net>
2019-09-24 12:06:57 -0500

    Update example_settings.py

--- example_settings.py
+++ example_settings.py
@@ -1,4 +1,4 @@
 #coding: utf-8
 from __future__ import unicode_literals
 
-DEFAULT_ACCOUNT = 'camilo.forero@aiesec.net' #The account that will be used, by default, to use the API. Its password should be saved in the database, using the admin interface
+DEFAULT_ACCOUNT = 'immanager@aiesec.org.pa' #The account that will be used, by default, to use the API. Its password should be saved in the database, using the admin interface
